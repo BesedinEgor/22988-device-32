d5c4188a60e35a7bc3b6338db955442781a849c8
Egor Besedin <Besedinegor@mail.ru>
2021-12-26 22:15:56 +0300

    реализация кнопки возврата в топ на  js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     $('.slider__items').slick({
         arrows: true,
         dots: true,
@@ -7,3 +7,19 @@ $(function(){
         autoplaySpeed: 3000
     })
 });
+
+let upButton = document.querySelector('.up-button');
+window.onscroll = function () {
+    if (window.pageYOffset > 400) {
+        upButton.classList.add('shown');
+    } else {
+        upButton.classList.remove('shown');
+    }
+}
+upButton.onclick = function () {
+    window.scrollTo(0, 0);
+    window.scrollBy({
+        top: 1,
+        behavior: 'smooth'
+    });
+}
